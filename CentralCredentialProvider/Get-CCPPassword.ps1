cdbda39ced529347b21ec81facab433233bef1a6
Pete Maan <pete.maan+github@gmail.com>
2018-04-04 23:33:54 +0100

    UPDATE Get-CCPPassword
    Added ability (and parametersets) to specify credentials or use default
    credentials for Invoke-RestMethod Call, as per example code in a
    CyberArk solution for scenarios where OS User authentication is
    configured for CCP. Original capability remains unchanged.
    
    Added methods to output object to convert password to secure string, or
    convert username & password to a credential object.
    
    Added additional examples for new functionality.

--- CentralCredentialProvider/Get-CCPPassword.ps1
+++ CentralCredentialProvider/Get-CCPPassword.ps1
@@ -41,6 +41,12 @@
 	The timeout is calculated when the request is sent from the web service to the Vault and returned back
 	to the web service.
 
+	.PARAMETER Credential
+	Specify the credentials object if OS User authentication is required for CCP.
+
+	.PARAMETER UseDefaultCredentials
+	Use the default credentials for CCP OS User authentication.
+
 	.PARAMETER WebServiceName
 	The name the CCP WebService is configured under in IIS.
 	Defaults to AIMWebService
@@ -49,26 +55,58 @@
 	The URL for the CCP Host
 
 	.EXAMPLE
-	Get-CCPPassword -AppID PSScript -Safe PSAccounts -Object PSPlatform-AccountName `
-		-URL https://cyberark.yourcompany.com
+	Get-CCPPassword -AppID PSScript -Safe PSAccounts -Object PSPlatform-AccountName -URL https://cyberark.yourcompany.com
 
 	Uses the PSScript App ID to retrieve password for the PSPlatform-AccountName object in the PSAccounts safe from the
 	https://cyberark.yourcompany.com/AIMWebService CCP Web Service.
 
 	.EXAMPLE
-	Get-CCPPassword -AppID PowerShell -Safe PSAccounts -UserName svc-psProvision -WebServiceName DevAIM `
-		-URL https://cyberark-dev.yourcompany.com
+	Get-CCPPassword -AppID PowerShell -Safe PSAccounts -UserName svc-psProvision -WebServiceName DevAIM -URL https://cyberark-dev.yourcompany.com
 
 	Uses the PowerShell App ID to search for and retrieve the password for the svc-psProvision account in the PSAccounts safe
 	from the https://cyberark-dev.yourcompany.com/DevAIM CCP Web Service.
 
+	.EXAMPLE
+	$result = Get-CCPPassword -AppID PS -Safe PS -Object PSP-AccountName -URL https://cyberark.yourcompany.com
+
+	$result.ToSecureSting()
+
+	Returns the password retrieved from CCP as a Secure String
+
+	.EXAMPLE
+	$result = Get-CCPPassword -AppID PS -Safe PS -Object PSP-AccountName -URL https://cyberark.yourcompany.com
+
+	$result.ToCredential()
+
+	Returns the username & password retrieved from CCP as a PSCredential object
+
+	.EXAMPLE
+	Get-CCPPassword -AppID PS -Safe PS -Object PSP-AccountName -URL https://cyberark.yourcompany.com -UseDefaultCredentials
+
+	Calls Invoke-RestMethod with the UseDefaultCredentials switch to use OS User authentication
+
+	.EXAMPLE
+	Get-CCPPassword -AppID PS -Safe PS -Object PSP-AccountName -URL https://cyberark.yourcompany.com -Credential $creds
+
+	Calls Invoke-RestMethod with the supplied Credentials for OS User authentication
 	#>
-	[CmdletBinding()]
+	[CmdletBinding(DefaultParameterSetName = "Default")]
 	Param(
 		# Unique ID of the application
 		[Parameter(
 			Mandatory = $true,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$AppID,
@@ -76,7 +114,18 @@
 		# Safe name
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$Safe,
@@ -84,7 +133,18 @@
 		# Folder name
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$Folder,
@@ -92,7 +152,18 @@
 		# Object name
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$Object,
@@ -100,7 +171,18 @@
 		# Search username
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$UserName,
@@ -108,7 +190,18 @@
 		# Search address
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$Address,
@@ -116,7 +209,18 @@
 		# Search database
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$Database,
@@ -124,7 +228,18 @@
 		# SetPolicyID format
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$PolicyID,
@@ -132,7 +247,18 @@
 		# Reason to record in audit log
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$Reason,
@@ -140,15 +266,56 @@
 		# Number of seconds to try
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[int]
 		$ConnectionTimeout,
 
+		# Credentials to send in request to CCP
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[ValidateNotNullOrEmpty()]
+		[PSCredential]
+		$Credential,
+
+		# Use current system credentials for request to CCP
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
+		)]
+		[Switch]
+		$UseDefaultCredentials,
+
 		# Unique ID of the CCP webservice in IIS
 		[Parameter(
 			Mandatory = $false,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$WebServiceName = "AIMWebService",
@@ -156,15 +323,29 @@
 		# CCP URL
 		[Parameter(
 			Mandatory = $true,
-			ValueFromPipelineByPropertyName = $true
+			ValueFromPipelineByPropertyName = $true,
+			ParameterSetName = "Default"
+		)]
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true,
+			ParameterSetName = "Credential"
+		)]
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true,
+			ParameterSetName = "DefaultCredentials"
 		)]
 		[string]
 		$URL
 	)
+
 	Begin {
+
+		#Collection of parameters which are to be excluded from the request URL
 		[array]$CommonParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
 		[array]$CommonParameters += [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
-		[array]$CommonParameters += "URL", "WebServiceName"
+		[array]$CommonParameters += "URL", "WebServiceName", "Credential", "UseDefaultCredentials"
 
 		#If Tls12 Security Protocol is available
 		if(([Net.SecurityProtocolType].GetEnumNames() -contains "Tls12") -and
@@ -172,6 +353,7 @@
 			#And Tls12 is not already in use
 			(-not ([System.Net.ServicePointManager]::SecurityProtocol -match "Tls12"))) {
 
+			#Use Tls12
 			Write-Verbose "Setting Security Protocol to TLS12"
 			[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
@@ -181,30 +363,71 @@
 
 	Process {
 
+		#Enumerate bound parameters to build query string for URL
 		$PSBoundParameters.keys | Where-Object {$CommonParameters -notcontains $_} | ForEach-Object {
 
 			[array]$QueryArgs += "$_=$([System.Uri]::EscapeDataString($PSBoundParameters[$_]))"
 
 		}
 
+		#Format URL query string
 		$Query = $QueryArgs -join '&'
 
-		$URI = "$URL/$WebServiceName/api/Accounts?$Query"
+		#Create hashtable of request parameters
+		$Request = @{
+			"URI"         = "$URL/$WebServiceName/api/Accounts?$Query"
+			"Method"      = "GET"
+			"ContentType" = "application/json"
+			"ErrorAction" = "Stop"
+		}
+
+		#Add Credential to request is provided
+		If($($PSCmdlet.ParameterSetName) -eq "Credential") {
+
+			$Request["Credential"] = $Credential
+
+		}
+
+		#Add UseDefaultCredentials switch to request if specified
+		ElseIf($($PSCmdlet.ParameterSetName) -eq "DefaultCredentials") {
+
+			$Request["UseDefaultCredentials"] = $true
+
+		}
 
 		Try {
 
-			$result = Invoke-RestMethod -Method GET -ContentType "application/json" -Uri $URI -ErrorAction Stop
+			#send request
+			$result = Invoke-RestMethod @Request
 
 		} Catch {
 
 			try {
 
+				#output error message
 				$err = $_ | ConvertFrom-Json -ErrorAction SilentlyContinue
 				Write-Error -Message $err.ErrorMsg -ErrorId $err.ErrorCode
 
+				#if JSON conversion fails throw original exception
 			} catch {throw $error[-1]}
 
-		} Finally {$result}
+			#output result
+		} Finally {
+
+			#Add ScriptMethod to output object to convert password to Secure String
+			$result | Add-Member -MemberType ScriptMethod -Name ToSecureString -Value {
+				$this.Content | ConvertTo-SecureString -AsPlainText -Force
+			}
+
+			#Add ScriptMethod to output object to convert username & password to Credential Object
+			$result | Add-Member -MemberType ScriptMethod -Name ToCredential -Value {
+				New-Object System.Management.Automation.PSCredential($this.UserName, $this.ToSecureString())
+			}
+
+			#Return the result from CCP
+			$result
+
+		}
 
 	}
 
